013c5cdd0878f05071febf8fe47ad3341d560365
root <root@>
2021-01-16 18:23:01 +0300

    payment4

--- js/api.js
+++ js/api.js
@@ -137,3 +137,167 @@ function addOrder(suffix) {
 	console.log(data);
 	xhr.send(data);
 }
+
+function addOrder2(suffix) {
+	//document.getElementById("send_rur2").disabled = true;
+	//document.getElementById("send_ntz2").disabled = true;
+	var xhr = new XMLHttpRequest();
+	var url = "/";
+	var name = document.getElementById("userName2"+suffix);
+	var phone = document.getElementById("userPhone2"+suffix);
+	var email = document.getElementById("userEmail2"+suffix);
+	var ticketsNum = document.getElementById("ticketsNum2"+suffix);
+
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			var json = JSON.parse(xhr.responseText);
+			if(json.startsWith('ERROR') ) {
+				//document.getElementById("send_rur2").disabled = false;
+				//document.getElementById("send_ntz2").disabled = false;
+				alert(json)
+			}
+			else {
+				window.location = json;
+			}
+		}
+	};
+	var lead_request = {};
+	var items=[];
+	var els = document.getElementsByClassName("items_quantity2");
+	Array.from(els).forEach((el) => {
+		items.push({name:el.id, quantity:el.value});
+	});
+	lead_request['type']="add_order";
+	lead_request['order']={};
+	lead_request['order']['name']=name.value;
+	lead_request['order']['phone']=phone.value;
+	lead_request['order']['email']=email.value;
+	lead_request['order']['orderType']=suffix;
+	lead_request['order']['delivery_text']='';
+	lead_request['order']['items']=items;
+	lead_request['order']['ticketsNum']=ticketsNum.value;
+
+	if (suffix == "FormNTZ") {
+		if (document.getElementById("userLogin2"+suffix).value != ""){
+			lead_request['order']['ntzLogin']=document.getElementById("userLogin2"+suffix).value;
+		}
+		else{
+			alert("You must set NTZ login");
+		}
+	}
+
+	var data = JSON.stringify(lead_request);
+	xhr.send(data);
+}
+
+
+function addOrder3(suffix) {
+	//document.getElementById("send_rur3").disabled = true;
+	//document.getElementById("send_ntz3").disabled = true;
+	var xhr = new XMLHttpRequest();
+	var url = "/";
+	var name = document.getElementById("userName3"+suffix);
+	var phone = document.getElementById("userPhone3"+suffix);
+	var email = document.getElementById("userEmail3"+suffix);
+	var ticketsNum = document.getElementById("ticketsNum3"+suffix);
+
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			var json = JSON.parse(xhr.responseText);
+			if(json.startsWith('ERROR') ) {
+				//document.getElementById("send_rur3").disabled = false;
+				//document.getElementById("send_ntz3").disabled = false;
+				alert(json)
+			}
+			else {
+				window.location = json;
+			}
+		}
+	};
+	var lead_request = {};
+	var items=[];
+	var els = document.getElementsByClassName("items_quantity3");
+	Array.from(els).forEach((el) => {
+		items.push({name:el.id, quantity:el.value});
+	});
+	lead_request['type']="add_order";
+	lead_request['order']={};
+	lead_request['order']['name']=name.value;
+	lead_request['order']['phone']=phone.value;
+	lead_request['order']['email']=email.value;
+	lead_request['order']['orderType']=suffix;
+	lead_request['order']['delivery_text']='';
+	lead_request['order']['items']=items;
+	lead_request['order']['ticketsNum']=ticketsNum.value;
+
+	if (suffix == "FormNTZ") {
+		if (document.getElementById("userLogin3"+suffix).value != ""){
+			lead_request['order']['ntzLogin']=document.getElementById("userLogin3"+suffix).value;
+		}
+		else{
+			alert("You must set NTZ login");
+		}
+	}
+
+	var data = JSON.stringify(lead_request);
+	xhr.send(data);
+}
+
+
+function addOrder4(suffix) {
+	//document.getElementById("send_rur4").disabled = true;
+	//document.getElementById("send_ntz4").disabled = true;
+	var xhr = new XMLHttpRequest();
+	var url = "/";
+	var name = document.getElementById("userName4"+suffix);
+	var phone = document.getElementById("userPhone4"+suffix);
+	var email = document.getElementById("userEmail4"+suffix);
+	var ticketsNum = document.getElementById("ticketsNum4"+suffix);
+
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			var json = JSON.parse(xhr.responseText);
+			if(json.startsWith('ERROR') ) {
+				//document.getElementById("send_rur4").disabled = false;
+				//document.getElementById("send_ntz4").disabled = false;
+				alert(json)
+			}
+			else {
+				window.location = json;
+			}
+		}
+	};
+	var lead_request = {};
+	var items=[];
+	var els = document.getElementsByClassName("items_quantity4");
+	Array.from(els).forEach((el) => {
+		items.push({name:el.id, quantity:el.value});
+	});
+	lead_request['type']="add_order";
+	lead_request['order']={};
+	lead_request['order']['name']=name.value;
+	lead_request['order']['phone']=phone.value;
+	lead_request['order']['email']=email.value;
+	lead_request['order']['orderType']=suffix;
+	lead_request['order']['delivery_text']='';
+	lead_request['order']['items']=items;
+	lead_request['order']['ticketsNum']=ticketsNum.value;
+
+	if (suffix == "FormNTZ") {
+		if (document.getElementById("userLogin4"+suffix).value != ""){
+			lead_request['order']['ntzLogin']=document.getElementById("userLogin4"+suffix).value;
+		}
+		else{
+			alert("You must set NTZ login");
+		}
+	}
+
+	var data = JSON.stringify(lead_request);
+	xhr.send(data);
+}
--- js/base.js
+++ js/base.js
@@ -1,24 +1,88 @@
 $(document).ready(function(){
-   /*  OPEN FORM */
-    $('#buyBtnRUR').on('click', function() {
-		$('#paymentMethod').fadeOut('100');
-		$('#paymentFormRUR').fadeIn('100');
-	});
-    $('#buyBtnNTZ').on('click', function() {
-		$('#paymentMethod').fadeOut('100');
-		$('#paymentFormNTZ').fadeIn('100');
+    /*  OPEN FORM */
+     $('#buyBtnRUR').on('click', function() {
+         $('#paymentMethod').fadeOut('100');
+         $('#paymentFormRUR').fadeIn('100');
+     });
+     $('#buyBtnNTZ').on('click', function() {
+         $('#paymentMethod').fadeOut('100');
+         $('#paymentFormNTZ').fadeIn('100');
+     });
+ 
+     /* OPEN METHOD */
+     $('#paymentMethodLink').on('click', function() {        
+         $('#paymentMethod').fadeIn('100');
+         $('#paymentFormRUR').fadeOut('100');
+     });
+     $('#paymentMethodLink2').on('click', function() {        
+         $('#paymentMethod').fadeIn('100');
+         $('#paymentFormNTZ').fadeOut('100');
+     });
+ 
+
+    /*  OPEN FORM2 */
+    $('#buyBtnRUR2').on('click', function() {
+        $('#payment2Method').fadeOut('100');
+        $('#payment2FormRUR').fadeIn('100');
+    });
+    $('#buyBtnNTZ2').on('click', function() {
+        $('#payment2Method').fadeOut('100');
+        $('#payment2FormNTZ').fadeIn('100');
     });
 
-    /* OPEN METHOD */
-    $('#paymentMethodLink').on('click', function() {        
-		$('#paymentMethod').fadeIn('100');
-		$('#paymentFormRUR').fadeOut('100');
+    /* OPEN METHOD2 */
+    $('#payment2MethodLink').on('click', function() {        
+        $('#payment2Method').fadeIn('100');
+        $('#payment2FormRUR').fadeOut('100');
     });
-    $('#paymentMethodLink2').on('click', function() {        
-		$('#paymentMethod').fadeIn('100');
-		$('#paymentFormNTZ').fadeOut('100');
+    $('#payment2MethodLink2').on('click', function() {        
+        $('#payment2Method').fadeIn('100');
+        $('#payment2FormNTZ').fadeOut('100');
     });
 
-    /* FOOTER */
-    $("#year").html(new Date().getFullYear());
-})
+
+    /*  OPEN FORM3 */
+    $('#buyBtnRUR3').on('click', function() {
+        $('#payment3Method').fadeOut('100');
+        $('#payment3FormRUR').fadeIn('100');
+    });
+    $('#buyBtnNTZ3').on('click', function() {
+        $('#payment3Method').fadeOut('100');
+        $('#payment3FormNTZ').fadeIn('100');
+    });
+
+    /* OPEN METHOD3 */
+    $('#payment3MethodLink').on('click', function() {        
+        $('#payment3Method').fadeIn('100');
+        $('#payment3FormRUR').fadeOut('100');
+    });
+    $('#payment3MethodLink2').on('click', function() {        
+        $('#payment3Method').fadeIn('100');
+        $('#payment3FormNTZ').fadeOut('100');
+    });
+
+
+    /*  OPEN FORM4 */
+    $('#buyBtnRUR4').on('click', function() {
+        $('#payment4Method').fadeOut('100');
+        $('#payment4FormRUR').fadeIn('100');
+    });
+    $('#buyBtnNTZ4').on('click', function() {
+        $('#payment4Method').fadeOut('100');
+        $('#payment4FormNTZ').fadeIn('100');
+    });
+
+    /* OPEN METHOD4 */
+    $('#payment4MethodLink').on('click', function() {        
+        $('#payment4Method').fadeIn('100');
+        $('#payment4FormRUR').fadeOut('100');
+    });
+    $('#payment4MethodLink2').on('click', function() {        
+        $('#payment4Method').fadeIn('100');
+        $('#payment4FormNTZ').fadeOut('100');
+    });
+
+
+     /* FOOTER */
+     $("#year").html(new Date().getFullYear());
+ })
